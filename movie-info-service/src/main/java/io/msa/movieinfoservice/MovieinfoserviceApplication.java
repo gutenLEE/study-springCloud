b2930589f05d25fffe29d96be5868888192d6ffb
gutenLEE <bnm1128@gmail.com>
2021-04-21 17:47:42 +0900

    set 3 micro-services to register Eureka-server

--- movie-info-service/src/main/java/io/msa/movieinfoservice/MovieinfoserviceApplication.java
+++ movie-info-service/src/main/java/io/msa/movieinfoservice/MovieinfoserviceApplication.java
@@ -2,8 +2,10 @@ package io.msa.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieinfoserviceApplication {
 
     public static void main(String[] args) {
